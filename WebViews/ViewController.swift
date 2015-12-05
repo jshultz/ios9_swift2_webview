612a37bc3ec1a50853e828fa106ab79df6c2b69d
Jason Shultz <jasshultz@gmail.com>
2015-12-05 09:27:36 -0700

    init commit

--- WebViews/ViewController.swift
+++ WebViews/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var webview: UIWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        var url = NSURL(string: "https://jasonshultz.com")
+//        
+//        var request = NSURLRequest(URL: url!)
+//        
+//        webview.loadRequest(request)
+        
+        var html = "<html><body><h1>my web page</h1><p>my web content.</p></body></html>"
+        
+        webview.loadHTMLString(html, baseURL: nil)
     }
 
     override func didReceiveMemoryWarning() {
